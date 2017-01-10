c25528f5a907dc97428e7f789d603386d932b0b6
malgorzata2017 <malgorzata.liszka@gmail.com>
2017-01-10 13:12:39 +0100

    Algorytm backpropagation

--- MyNeuralNetwork.m
+++ MyNeuralNetwork.m
@@ -50,3 +50,42 @@ pause;
 lambda = 3;
 checkNNGradients(lambda);
 
+
+fprintf('\nTrenowanie sieci nueronowej... \n')
+
+options = optimset('MaxIter', 50);
+
+lambda = 1;
+
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+fprintf('Program sie zatrzymal. Nacisnij enter aby kontynuowac.\n');
+pause;
+
+fprintf('\nWizualizacja sieci neuronowej... \n')
+
+displayData(Theta1(:, 2:end));
+
+fprintf('Program sie zatrzymal. Nacisnij enter aby kontynuowac.\n');
+pause;
+
+pred = predict(Theta1, Theta2, X);
+
+fprintf('\nDokladnosc przewidywan sieci: %f\n', mean(double(pred == y)) * 100);
+
+fprintf('\nPrzyklad dzialania na losowych przykladach');
+
+
+
+
