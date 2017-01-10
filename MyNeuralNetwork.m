78bfda9c5a0892e2989c7a3dca3ad23f9eca7c02
malgorzata2017 <malgorzata.liszka@gmail.com>
2017-01-10 13:11:37 +0100

    dodanie wyswietlania obrazkow i ladowanie wag

--- MyNeuralNetwork.m
+++ MyNeuralNetwork.m
@@ -6,3 +6,22 @@ hidden_layer_size = 25;   % 25 ukrytych jednostek
 num_labels = 10;          % 10 etykiet, od 1 do 10   
 
 fprintf('Ladowanie i wyswietlanie danych ...\n')
+
+
+load('data1.mat');
+m = size(X, 1);
+
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+
+displayData(X(sel, :));
+
+fprintf('Program sie zatrzymal. Nacisnij enter aby kontynuowac.\n');
+pause;
+
+
+fprintf('\nLadowanie poczatkowych wag ...\n')
+
+load('weights.mat');
+
+nn_params = [Theta1(:) ; Theta2(:)];
