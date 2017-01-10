6d0c4307e1e91f35eed2947b60afec02b544081a
malgorzata2017 <malgorzata.liszka@gmail.com>
2017-01-10 13:12:17 +0100

    Przejscie sieci w przod

--- MyNeuralNetwork.m
+++ MyNeuralNetwork.m
@@ -25,3 +25,28 @@ fprintf('\nLadowanie poczatkowych wag ...\n')
 load('weights.mat');
 
 nn_params = [Theta1(:) ; Theta2(:)];
+
+% Przejscie sieci neuronowej w przod
+
+lambda = 1;
+
+J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
+                   num_labels, X, y, lambda);
+
+g = sigmoidGradient([1 -0.5 0 0.5 1]);
+
+fprintf('\nInicjalizowanie parametrow sieci neuronowej ...\n')
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+checkNNGradients;
+
+fprintf('Program sie zatrzymal. Nacisnij enter aby kontynuowac.\n');
+pause;
+
+lambda = 3;
+checkNNGradients(lambda);
+
